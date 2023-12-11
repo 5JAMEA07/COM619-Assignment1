5d1d3ffe1e2da9b885ae512a77c4fa083705e484
Nathan Worlock <nathanthaddeusworlock@hotmail.co.uk>
2023-12-11 15:03:57 +0000

    sign up form

---
+++ view/src/Signup.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Signup() {
+    return (
+        <div className="Signup">
+            <form>
+                <label for="firstname">First name:</label>
+                <input type='text' id='firstname' />
+                <label for="lastname">Last name:</label>
+                <input type='text' id='lastname' />
+                <label for="usernamename">Username:</label>
+                <input type='text' id='username' />
+                <label for="password">password:</label>
+                <input type='password' id='password' />
+            </form>
+        </div>
+    );
+}
+
+export default Signup;
