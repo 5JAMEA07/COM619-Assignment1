36cd8583cec1318d12f462f55726fbaa63ee4208
Nathan Worlock <nathanthaddeusworlock@hotmail.co.uk>
2023-12-13 17:00:52 +0000

    updated POI to use created API

--- view/src/POIList.jsx
+++ view/src/POIList.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 async function PoiList() {
     const [results, setResults] = React.useState([]);
+    const username = sessionStorage.getItem('user.username');
 
     React.useEffect( () => {
-        fetch(`/api/getUserPOIs/${sessionStorage.getItem('user.username')}`)
+        fetch(`/api/getAllMapPointsForUser`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username
+        })
+
+    })
             .then(setResults);
     })
-
     //modify above code to get POI's if endpoint is added
     //modify below form action to edit POI once endpoint is added
 
-    const PoisHtml = results.map(Poi => <form action="/api/editPOI/:id" method="POST"> <li key={Poi.id}><input type="text" id="name" value={Poi.id}/>, <input type="text" id="description" value={Poi.description}/>, <input type="text" id="lat" value={Poi.lat}/>, <input type="text" id="lng" value={Poi.lng}/>, <input value = {Poi.PhotoUrl} type="file" id="img" name="img" accept="image/*"/> <input type="submit" value="Submit"/></li> </>);
+    const PoisHtml = results.map(Poi => <form action='/api/updateMapPointWithImage' method="POST"> <li key={Poi.id}><input type="hidden" id="say" value={Poi.id} /> <input type="text" id="name" value={Poi.id}/>, <input type="text" id="description" value={Poi.description}/>, <input type="text" id="lat" value={Poi.lat}/>, <input type="text" id="lng" value={Poi.lng}/>, <input value = {Poi.PhotoUrl} type="file" id="img" name="img" accept="image/*"/> <input type="submit" value="Submit"/></li> </>);
 
 
     return (
